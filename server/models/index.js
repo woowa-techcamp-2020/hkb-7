9d96c2aca57030a4ab8a9fe79c3c8715e8f4b75f
younho9 <younho9.choo@gmail.com>
2020-07-30 02:17:56 +0900

    feat: models 디렉토리의 index 역할 수정

--- server/models/index.js
+++ server/models/index.js
@@ -1,13 +1,9 @@
-const mysql = require('mysql2');
+function init() {
+  require('./activity');
+  require('./category');
+  require('./payment-method');
+  require('./profile');
+  require('./user');
+}
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
-module.exports = pool;
+module.exports = { init };
