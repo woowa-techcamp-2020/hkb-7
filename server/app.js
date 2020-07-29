25a70eeb4b2347d9687bb50049b92a2c5d01bed3
younho9 <younho9.choo@gmail.com>
2020-07-30 02:07:01 +0900

    feat: 에러 메시지 출력과 에러 상태 코드 전송

--- server/app.js
+++ server/app.js
@@ -27,8 +27,12 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
+  console.log(err);
+  console.log(err.message);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  if (err.message.includes('undefined')) err.status = 400;
 
   // render the error page
   res.status(err.status || 500);
