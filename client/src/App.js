ddac6b2e22ba7534a33ccb49e36405a4b44ed27e
younho9 <younho9.choo@gmail.com>
2020-07-30 13:13:21 +0900

    chore: 불필요한 예제 파일 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import Page from 'pages/Page';
-import './style.scss';
+import './styles/common.scss';
 
 export default class App {
   constructor($target) {
     this.$target = $target;
-    this.$page = new Page();
   }
 }
