99ae49c1a4798dbc717e3a2135595612d64c651e
younho9 <younho9.choo@gmail.com>
2020-07-30 21:32:22 +0900

    feat: ActivityTable 컴포넌트 html 템플릿 작성

---
+++ client/src/components/ActivityTable/index.js
@@ -0,0 +1,56 @@
+import './styles.scss';
+
+export default function ActivityTable() {
+  return `
+    <div class="activity-table">
+      <div class="date-summary">
+        <div class="date-container">
+          <div>6월 16일</div>
+          <div>화</div>
+        </div>
+        <div class="summary-container">
+          <div>
+            <div>+0원</div>
+            <div>-26,000원</div>
+          </div>
+        </div>
+      </div>
+      <div class="activity-row">
+        <div class="activity-category-tag">쇼핑/뷰티</div>
+        <div class="activity-content">미용실</div>
+        <div class="activity-payment-method">현대카드</div>
+        <div class="activity-price outcome">-20,000원</div>
+      </div>
+      <div class="activity-row">
+        <div class="activity-category-tag">식비</div>
+        <div class="activity-content">맥도날드</div>
+        <div class="activity-payment-method">카카오체크카드</div>
+        <div class="activity-price outcome">-6,000원</div>
+      </div>
+      <div class="date-summary">
+        <div class="date-container">
+          <div>6월 15일</div>
+          <div>월</div>
+        </div>
+        <div class="summary-container">
+          <div>
+            <div>+2,750,000원</div>
+            <div>-17,200원</div>
+          </div>
+        </div>
+      </div>
+      <div class="activity-row">
+        <div class="activity-category-tag">월급</div>
+        <div class="activity-content">월급</div>
+        <div class="activity-payment-method">국민은행</div>
+        <div class="activity-price outcome">+2,750,000원</div>
+      </div>
+      <div class="activity-row">
+        <div class="activity-category-tag">생활</div>
+        <div class="activity-content">이마트에서 생필품</div>
+        <div class="activity-payment-method">현대카드</div>
+        <div class="activity-price outcome">-17,200</div>
+      </div>
+    </div>
+  `;
+}
