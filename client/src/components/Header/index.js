bbad946c888fa3dfc11fdfdf50440241adf7885a
younho9 <younho9.choo@gmail.com>
2020-07-31 11:51:48 +0900

    feat: 컴포넌트를 클래스 형태로 변경하고 render 함수 추가

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,22 @@
 import './styles.scss';
+import { element } from 'utils/element';
 
-export default function Header() {
-  return `
-    <header class="header">
-      <div class="header-title">우아한 가계부</div>
-      <button class="payment-manage-button">결제 수단 관리</button>
-    </header>
-  `;
+export default class Header {
+  constructor($target) {
+    this.$target = $target;
+
+    this.$Header = element('header', {
+      className: 'header',
+      innerHTML: `
+        <div class="header-title">우아한 가계부</div>
+        <button class="payment-manage-button">결제 수단 관리</button>
+      `,
+    });
+
+    this.render();
+  }
+
+  render() {
+    this.$target.appendChild(this.$Header);
+  }
 }
