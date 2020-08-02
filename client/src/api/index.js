a0da1e2f463de990a315729baa23bb6391398e42
younho9 <younho9.choo@gmail.com>
2020-08-02 20:34:35 +0900

    feat: 기본 API method 함수 작성

---
+++ client/src/api/index.js
@@ -0,0 +1,35 @@
+const defaultOptions = (method) => ({
+  method,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const serverUrl = 'http://localhost:3000';
+
+const createQuery = (data) => {
+  return data
+    ? '?' +
+        Object.keys(data)
+          .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(data[k]))
+          .join('&')
+    : '';
+};
+
+const POST = async (url = '', data) =>
+  await fetch(`${serverUrl}${url}`, {
+    body: JSON.stringify(data),
+    ...defaultOptions('POST'),
+  });
+
+const PUT = async (url = '', data) =>
+  await fetch(`${serverUrl}${url}`, {
+    body: JSON.stringify(data),
+    ...defaultOptions('PUT'),
+  });
+
+const GET = async (url = '', data) => await fetch(`${serverUrl}${url}${createQuery(data)}`, defaultOptions('GET'));
+
+const DELETE = async (url = '') => await fetch(`${serverUrl}${url}`, defaultOptions('DELETE'));
+
+export default {};
