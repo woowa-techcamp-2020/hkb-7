7c7d0bf27c45775d94116d88433aa658170a3b81
younho9 <younho9.choo@gmail.com>
2020-08-02 21:57:37 +0900

    refactor: MainPage의 메서드 수정

--- client/src/views/pages/MainPage/index.js
+++ client/src/views/pages/MainPage/index.js
@@ -12,20 +12,41 @@ export default class MainPage {
   constructor($target) {
     this.$App = $target;
     this.store = store;
+    this.store.subscribe((data) => this.init(data));
 
+    this.createHeader();
+    this.createNavigator();
+    this.createSection();
+  }
+
+  init(data) {
+    this.$Header.init();
+    this.$MonthNavigator.render(data);
+    this.$SectionNavigator.render(data);
+    this.$ActivitySection.init(data);
+  }
+
+  createHeader() {
+    this.$Header = new Header(this.$App);
+  }
+
+  createNavigator() {
     this.$Container = element('div', {
       className: 'main-container',
     });
-    this.$Header = new Header(this.$App);
-
-    this.render();
-  }
-
-  render() {
     this.$App.appendChild(this.$Container);
+
     this.$MonthNavigator = new MonthNavigator(this.$Container);
     this.$SectionNavigator = new SectionNavigator(this.$Container);
-    this.$ActivitySection = new ActivitySection(this.$Container);
+  }
+
+  createSection() {
+    this.$SectionContainer = element('div', {
+      className: 'section-container',
+    });
+    this.$Container.appendChild(this.$SectionContainer);
+
+    this.$ActivitySection = new ActivitySection(this.$SectionContainer);
   }
 
   setState() {}
