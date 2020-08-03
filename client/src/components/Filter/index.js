ae3f6054bbd12f81f3a1874be96e3b2c186dc7eb
younho9 <younho9.choo@gmail.com>
2020-08-03 13:54:42 +0900

    feat: 수입, 지출 합계를 계산하고 view에 반영하는 기능 추가

--- client/src/components/Filter/index.js
+++ client/src/components/Filter/index.js
@@ -16,10 +16,10 @@ export default class Filter {
     this.$Filter.innerHTML = `
       <input type="checkbox" name="income-filter"/>
       <label for="income-filter">수입</label>
-      <div class="income-total">2,750,000원</div>
+      <div class="income-total">${data.total.income}원</div>
       <input type="checkbox" name="outcome-filter"/>
       <label for="outcome-filter">지출</label>
-      <div class="outcome-total">444,790원</div>
+      <div class="outcome-total">${data.total.outcome}원</div>
     `;
   }
 }
--- client/src/models/store.js
+++ client/src/models/store.js
@@ -8,6 +8,7 @@ class Store extends Observable {
       userId: 9,
       activities: [],
       currentMonth: null,
+      total: {},
     };
     this.init();
   }
@@ -15,22 +16,39 @@ class Store extends Observable {
   async init() {
     const currentMonth = new Date().getMonth() + 1;
     const activities = await apis.getActivities(this.data.userId, currentMonth);
-    this.data = { ...this.data, currentMonth: currentMonth, ...activities };
+    const total = this.calcTotal(activities);
+    this.data = { ...this.data, total, currentMonth: currentMonth, ...activities };
     this.notify(this.data);
   }
 
   async prevMonth() {
     const activities = await apis.getActivities(this.data.userId, --this.data.currentMonth);
-    this.data = { ...this.data, ...activities };
+    const total = this.calcTotal(activities);
+    this.data = { ...this.data, total, ...activities };
     this.notify(this.data, 'currentMonth');
     this.notify(this.data, 'activities');
   }
 
   async nextMonth() {
     const activities = await apis.getActivities(this.data.userId, ++this.data.currentMonth);
-    this.data = { ...this.data, ...activities };
+    const total = this.calcTotal(activities);
+    this.data = { ...this.data, total, ...activities };
     this.notify(this.data, 'currentMonth');
     this.notify(this.data, 'activities');
+  }
+
+  calcTotal({ activities }) {
+    return activities.reduce(
+      (acc, cur) => {
+        if (cur.is_income === 1) {
+          acc.income += cur.price;
+          return acc;
+        }
+        acc.outcome += cur.price;
+        return acc;
+      },
+      { income: 0, outcome: 0 },
+    );
   }
 }
 
