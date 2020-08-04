7fa01fb13d6ecd9b53a3078170b0acce30e47c59
younho9 <younho9.choo@gmail.com>
2020-08-05 02:23:51 +0900

    feat: SectionNavigator가 moveSection시 렌더링 되는 기능 구현

--- client/src/components/SectionNavigator/index.js
+++ client/src/components/SectionNavigator/index.js
@@ -7,8 +7,9 @@ export default class SectionNavigator {
   constructor($target) {
     this.$target = $target;
     this.store = store;
-    this.pathList = ['/activity/', '/calendar/', '/statistic/'];
+    this.store.subscribe((data) => this.render(data), 'moveSection');
 
+    this.pathList = ['/activity', '/calendar', '/statistic'];
     this.$SectionNavigator = element('ul', {
       className: 'section-navigator left-col',
     });
@@ -20,12 +21,12 @@ export default class SectionNavigator {
     this.$SectionNavigator.innerHTML = `
           <li class="section-navigator-item">
             <input
-              class="section-navigator-item-input"
+          class="section-navigator-item-input activity"
               type="radio"
               name="section-navigator-item-input"
               id = "section-navigator-item-input-activities"
               value = "activities"
-              checked
+          ${data.path === '/activity' ? 'checked' : ''}
             />
             <label class="section-navigator-item-label" for="section-navigator-item-input-activities">
               <svg class="side-icon" id="Capa_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><g><path d="m105 272h60c8.284 0 15-6.716 15-15v-60c0-8.284-6.716-15-15-15h-60c-8.284 0-15 6.716-15 15v60c0 8.284 6.716 15 15 15zm15-60h30v30h-30z"/><path d="m316 242h-90c-8.284 0-15 6.716-15 15s6.716 15 15 15h90c8.284 0 15-6.716 15-15s-6.716-15-15-15z"/><path d="m316 302h-210c-8.284 0-15 6.716-15 15s6.716 15 15 15h210c8.284 0 15-6.716 15-15s-6.716-15-15-15z"/><path d="m316 362h-210c-8.284 0-15 6.716-15 15s6.716 15 15 15h210c8.284 0 15-6.716 15-15s-6.716-15-15-15z"/><path d="m226 422h-120c-8.284 0-15 6.716-15 15s6.716 15 15 15h120c8.284 0 15-6.716 15-15s-6.716-15-15-15z"/><path d="m476.604 94.391c-95.526-95.489-89.99-90.082-91.086-90.983-2.581-2.117-5.929-3.408-9.518-3.408h-240c-8.284 0-15 6.716-15 15v77h-75c-8.284 0-15 6.716-15 15v390c0 8.284 6.716 15 15 15h330c8.284 0 15-6.716 15-15v-77h75c8.284 0 15-6.716 15-15v-300c0-4.18-1.748-7.963-4.396-10.609zm-85.604-43.178 38.787 38.787h-38.787zm-30 430.787h-300v-360h210v75c0 8.284 6.716 15 15 15h75zm-21.213-300h-38.787v-38.787zm51.213 208c-.028-205.813.068-193.065-.076-194.497-.353-3.563-1.994-6.787-4.32-9.112-96.752-95.91-91.265-94.391-100.604-94.391h-135v-62h210v75c0 8.284 6.716 15 15 15h75v270z"/></g></svg>
@@ -36,12 +37,12 @@ export default class SectionNavigator {
           </li>
           <li class="section-navigator-item">
             <input
-              class="section-navigator-item-input"
+          class="section-navigator-item-input calendar"
               type="radio"
               name="section-navigator-item-input"
               id = "section-navigator-item-input-calendar"
               value = "calendar"
-              
+          ${data.path === '/calendar' ? 'checked' : ''}
             />
             <label class="section-navigator-item-label" for="section-navigator-item-input-calendar">
               <svg class="side-icon" id="Capa_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><g><path d="m482 31h-45v-16c0-8.284-6.716-15-15-15s-15 6.716-15 15v16h-30v-16c0-8.284-6.716-15-15-15s-15 6.716-15 15v16h-181v-16c0-8.284-6.716-15-15-15s-15 6.716-15 15v16h-30v-16c0-8.284-6.716-15-15-15s-15 6.716-15 15v16h-46c-8.284 0-15 6.716-15 15v451c0 8.284 6.716 15 15 15h452c8.284 0 15-6.716 15-15 0-15.566 0-434.004 0-451 0-8.284-6.716-15-15-15zm-286 211v60h-60v-60zm90 0v60h-60v-60zm90 0v60h-60v-60zm91 0v60h-61v-60zm-181 90v60h-60v-60zm-180-30h-61v-60h61zm-61 30h61v60h-61zm91 0h60v60h-60zm60 90v60h-60v-60zm30 0h60v60h-60zm90 0h60v60h-60zm0-30v-60h60v60zm90-60h61v60h-61zm-361-271h31v15c0 8.284 6.716 15 15 15s15-6.716 15-15v-15h30v15c0 8.284 6.716 15 15 15s15-6.716 15-15v-15h181v15c0 8.284 6.716 15 15 15s15-6.716 15-15v-15h30v15c0 8.284 6.716 15 15 15s15-6.716 15-15v-15h30v151h-422zm0 361h61v60h-61zm361 60v-60h61v60z"/><path d="m166.5 152h179c8.284 0 15-6.716 15-15s-6.716-15-15-15h-179c-8.284 0-15 6.716-15 15s6.716 15 15 15z"/></g></svg>
@@ -52,12 +53,12 @@ export default class SectionNavigator {
           </li>
           <li class="section-navigator-item">
             <input
-              class="section-navigator-item-input"
+          class="section-navigator-item-input statistic"
               type="radio"
               name="section-navigator-item-input"
               id = "section-navigator-item-input-stats"
               value = "stats"
-              
+          ${data.path === '/statistic' ? 'checked' : ''}
             />
             <label class="section-navigator-item-label" for="section-navigator-item-input-stats">
               <svg class="side-icon" id="Capa_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><g><path d="m466 31h-195v-16c0-8.284-6.716-15-15-15s-15 6.716-15 15v16h-195c-8.284 0-15 6.716-15 15s6.716 15 15 15h15v255c0 24.813 20.187 45 45 45h11.469l-15.633 97.605c-4.489 28.028 17.182 53.395 45.523 53.395 22.776 0 41.922-16.323 45.523-38.812l17.969-112.188h90.175l17.969 112.188c3.602 22.489 22.747 38.812 45.524 38.812 28.384 0 50.006-25.409 45.523-53.395l-15.633-97.605h11.591c24.813 0 45-20.187 45-45v-255h15c8.284 0 15-6.716 15-15s-6.716-15-15-15zm-302.74 437.443c-1.258 7.855-7.945 13.557-15.9 13.557-9.854 0-17.483-8.778-15.9-18.65l16.392-102.35h32.617zm217.159-5.093c1.571 9.797-5.962 18.65-15.9 18.65-7.955 0-14.643-5.701-15.9-13.557l-17.21-107.443h32.617zm40.581-147.35c0 8.271-6.729 15-15 15h-300c-8.271 0-15-6.729-15-15v-255h330z"/><path d="m256 300.951c62.01 0 110.017-53.261 104.747-114.023-7.645-88.048-115.767-128.4-178.993-65.174-65.536 65.535-19.605 179.197 74.246 179.197zm62.573-63.592-29.194-29.195 41.075-3.558c-1.318 11.5-5.266 22.752-11.881 32.753zm9.33-62.643-56.903 4.929v-57.092c27.433 5.559 49.161 25.848 56.903 52.163zm-86.903-52.164c.025 79.912-.267 74.717.629 77.734 1.535 5.169.039 2.577 55.73 58.287-29.105 19.249-68.783 16.071-94.393-9.539-42.449-42.452-19.871-114.752 38.034-126.482z"/></g></svg>
@@ -103,7 +104,7 @@ export default class SectionNavigator {
 
     this.pathList.forEach((path) => {
       if (path !== data.path) {
-        bindEvent(`.section-tab.${path.slice(1, -1)}`, 'click', () => {
+        bindEvent(`.section-navigator-item-input.${path.slice(1)}`, 'click', () => {
           this.store.moveSection(path);
         });
       }
