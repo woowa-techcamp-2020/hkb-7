f804738140f79e7345a7bcbdc8eb02060afaebc2
Sunghyun Cho <mail@chosunghyun.com>
2020-08-04 18:53:27 +0900

    feat: filter 결합 성공

--- client/src/components/Filter/index.js
+++ client/src/components/Filter/index.js
@@ -18,12 +18,12 @@ export default class Filter {
 
     <input class="income-outcome-filter-input" type="checkbox" name="income-outcome-filter-input" id="income-filter-input" checked/>
     <label class="income-outcome-filter-label left-label" for="income-filter-input">
-      <div class="income-outcome-filter-label-text">✓ 수입 ${data.total.income}</div>
+      <div class="income-outcome-filter-label-text">✓ 수입 ${data.total.income}원</div>
     </label>
     
     <input class="income-outcome-filter-input" type="checkbox" name="income-outcome-filter-input" id="outcome-filter-input" checked/>
     <label class="income-outcome-filter-label right-label" for="outcome-filter-input">
-      <div class="income-outcome-filter-label-text">✓ 지출 ${data.total.outcome}</div>
+      <div class="income-outcome-filter-label-text">✓ 지출 ${data.total.outcome}원</div>
     </label>
     
     </div>
