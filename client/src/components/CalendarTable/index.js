0d840ae34d0400108dc72890fc6bedf6e595ab29
Sunghyun Cho <mail@chosunghyun.com>
2020-08-05 18:08:57 +0900

    feat: 캘린더 버그 수정 및 지출입 데이터 표시
    - 지난달과 다음달 날짜가 정상적으로 렌더링되지 않는 버그 수정
    - 지출입 데이터 표시

--- client/src/components/CalendarTable/index.js
+++ client/src/components/CalendarTable/index.js
@@ -1,4 +1,5 @@
 import './styles.scss';
+import { addLeadingZeros } from 'utils/helper';
 import { element } from 'utils/element';
 import { html } from 'utils/html';
 
@@ -26,10 +27,30 @@ export default class CalendarTable {
     };
   }
 
+  getTomorrow(date) {
+    let tomorrow = new Date();
+    return tomorrow.setDate(new Date().getDate() + 1);
+  }
+
+  highlightToday() {
+    let today = new Date();
+    let todayString = `${today.getFullYear()}-${addLeadingZeros(2, today.getMonth() + 1)}-${addLeadingZeros(
+      2,
+      today.getDate(),
+    )}`;
+    let todayCell = document.querySelector(`.date-${todayString}`);
+    if (todayCell) {
+      todayCell.classList.add('today');
+      todayCell.innerHTML += `<div class = "today todayLabel">today</div>`;
+    }
+  }
+
   render(data) {
+    console.log(data.activities);
     const date = this.processDate(data);
     let calendarContents = [];
     let days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+
     for (let d = 0; d < 7; d++) {
       calendarContents.push(
         html`<div class="calendar-table-day-bar">
@@ -39,27 +60,46 @@ export default class CalendarTable {
         </div>`,
       );
     }
+
     for (let d = 0; d < date.thisMonthStartDate.getDay(); d++) {
       calendarContents.push(
         html`<div class="calendar-table-cell not-this-month">
-          ${date.lastMonthLastDate.getDate() - date.thisMonthStartDate.getDay() + d}
+          ${date.lastMonthLastDate.getDate() - date.thisMonthStartDate.getDay() + d + 1}
         </div>`,
       );
     }
+
     for (let d = 0; d < date.thisMonthLastDate.getDate(); d++) {
+      let dayString = `${data.year}-${addLeadingZeros(2, data.month)}-${addLeadingZeros(2, d + 1)}`;
       calendarContents.push(html`<div class="calendar-table-cell">
-        <div>${d + 1}</div>
+        <div class="date-${dayString}">
+          ${d + 1}
+        </div>
+        <div class="calendar-table-cell-activity">
+          ${data.activities[dayString]
+            ? `
+          <div class = "positive">
+            ${data.activities[dayString].dailyTotal.income.toLocaleString('ko-KR')}원
+          </div>
+          <div class = "negative">
+            ${data.activities[dayString].dailyTotal.outcome.toLocaleString('ko-KR')}원
+          </div>`
+            : ''}
+        </div>
       </div>`);
     }
-    while (calendarContents.length % 7 != 0) {
-      calendarContents.push(
-        html`<div class="calendar-table-cell not-this-month">
-          ${(calendarContents.length % 7) - 1}
-        </div>`,
-      );
+
+    let nextMonthDaysToRender = 7 - (calendarContents.length % 7);
+    if (nextMonthDaysToRender < 7) {
+      for (let d = 0; d < nextMonthDaysToRender; d++) {
+        calendarContents.push(
+          html`<div class="calendar-table-cell not-this-month">
+            ${d + 1}
+          </div>`,
+        );
+      }
     }
-    console.log(calendarContents);
-    console.log(calendarContents.join(''));
     this.$CalendarTable.innerHTML = calendarContents.join('');
+    this.highlightToday();
   }
 }
