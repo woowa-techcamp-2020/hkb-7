42d397a78b9636a196e427907b70512e40f3db4b
Sunghyun Cho <mail@chosunghyun.com>
2020-08-07 01:45:32 +0900

    chore: 주석 제거

--- server/routes/category.js
+++ server/routes/category.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 const categoryController = require('../controllers/category');
 const { wrapAsync } = require('../../shared/utils/helper');
-// const { isLoggedIn } = require('../utils/auth');
 
-// router.use(isLoggedIn);
 router.get('/:token', wrapAsync(categoryController.findAll));
 
 module.exports = router;
