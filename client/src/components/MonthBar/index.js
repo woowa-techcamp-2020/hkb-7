ab7269df249db5bd4a93769a7b69dbc90b3700e4
younho9 <younho9.choo@gmail.com>
2020-08-06 20:51:13 +0900

    feat: MonthBar moveMonth와 clickFilter 이벤트 구독

--- client/src/components/MonthBar/index.js
+++ client/src/components/MonthBar/index.js
@@ -1,11 +1,15 @@
 import './styles.scss';
 import { element } from 'utils/element';
 import MonthNavigator from 'components/MonthNavigator';
+import { store } from 'models/store';
 import Filter from 'components/Filter';
 
 export default class MonthBar {
   constructor($target) {
     this.$target = $target;
+    this.store = store;
+    this.store.subscribe((data) => this.render(data), 'moveMonth');
+    this.store.subscribe((data) => this.render(data), 'clickFilter');
 
     this.$MonthBar = element('div', {
       className: 'month-bar',
