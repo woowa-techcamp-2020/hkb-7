fa4076ef95c08bc0f2378cec5adb90b1e1981886
Sunghyun Cho <mail@chosunghyun.com>
2020-08-07 01:49:08 +0900

    feat: 컨트롤러 레벨에서 post로 전달된 JWT를 readIdInJwt 헬퍼 함수로 해석하는 기능 구현

--- server/controllers/activity.js
+++ server/controllers/activity.js
@@ -1,20 +1,26 @@
 const Activity = require('../models/activity');
+const { readIdInJwt } = require('../utils/auth');
 
 exports.create = async (req, res) => {
+  const userId = readIdInJwt(req.body.token);
+  req.body.user_id = userId;
+  console.log(req.body)
   const activity = await Activity.create(req.body);
   res.send(activity);
 };
 
 exports.findAll = async (req, res) => {
+  const userId = readIdInJwt(req.params.token);
   const activities = await Activity.findAll(
     'activity.id, activity.price, activity.content, activity.date, category.name as category, category.is_income, payment_method.name as payment',
     {
       'YEAR(date)': req.params.year,
       'MONTH(date)': req.params.month,
-      'activity.user_id': req.params.id,
+      'activity.user_id': userId,
       'activity.is_active': 1,
     },
   );
+  console.log(activities)
   res.send({ activities });
 };
 
