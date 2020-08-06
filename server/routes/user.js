34f5fe19e5d8adced91894bac92042b00d6a115a
Sunghyun Cho <mail@chosunghyun.com>
2020-08-07 01:41:26 +0900

    feat: userRouter에 구글 로그인 콜백 주소 대응 코드 추가

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const userController = require('../controllers/user');
 const { wrapAsync } = require('../../shared/utils/helper');
-const { authenticateLocal, isAuthenticated } = require('../utils/auth');
+const { authenticateLocal, authenticateGoogleCallback } = require('../utils/auth');
 
 router.post('/', wrapAsync(userController.create));
-router.post('/login', authenticateLocal(), wrapAsync(userController.findById));
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+router.post('/login', authenticateLocal, wrapAsync(userController.findById));
 
-// router.use(isAuthenticated);
+// router.use(isLoggedIn);
+
 router.get('/:id', wrapAsync(userController.findById));
 router.get('/', wrapAsync(userController.findAll));
 router.put('/', wrapAsync(userController.update));
 router.delete('/:id', wrapAsync(userController.delete));
+
+router.get('/google/redirect', passport.authenticate('google'), (req, res, next) => {
+  const payload = { id: req.user.id };
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  res
+    .status(200)
+    .cookie('token', token, {
+      // expires after 7 days (1 week)
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    })
+    .redirect('/activity');
+});
 
 module.exports = router;
