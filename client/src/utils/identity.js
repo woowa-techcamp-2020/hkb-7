ff4414b9dd30f2518b7ae54bc04eca657433990d
younho9 <younho9.choo@gmail.com>
2020-08-07 11:25:30 +0900

    feat: token parse 수정

--- client/src/utils/identity.js
+++ client/src/utils/identity.js
@@ -3,15 +3,17 @@ export const identified = () => {
 };
 
 export const saveTokenInLocalStorage = (cookie) => {
-  if (!cookie) {
+  if (!cookie.includes('token')) {
     return;
   }
-  const token = cookie
-    .split('; ')
-    .find((row) => row.startsWith('token'))
-    .split('=')[1];
+  const tokenLoc = cookie.indexOf('token');
+  let endLoc = Math.min(cookie.length, cookie.indexOf(';', tokenLoc));
+  if (endLoc === -1) {
+    endLoc = cookie.length;
+  }
+  const token = cookie.substring(tokenLoc, endLoc + 1);
   if (!token) return;
-  localStorage.setItem('token', token);
+  localStorage.setItem('token', token.split('=')[1]);
   document.cookie = `token=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
 };
 
